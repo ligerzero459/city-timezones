3c43c8e9e0cb2543c55d8778fa5bc6273b9cdbe4
Ryan Mottley <ligerzero459@gmail.com>
2018-04-07 00:17:44 -0500

    Added new search functionality and small coding style changes
    - lookupViaState
    - lookupViaCityState
    - new optional fuzzy matching
    
    - removed inconsistant semi-colons
    - converted double-quotes to single-quotes to match
    
    Semver: minor

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
-"use strict";
-const _ = require('lodash')
+'use strict'
 const cityMapping = require('./data/cityMap.json')
 
-function lookupViaCity(city) {
-  const cityLookup = _.filter(cityMapping, function (o) { return o.city === city })
-  if (cityLookup && cityLookup.length > 0) {
-    return cityLookup
-  } else {
-    return []
-  }
+function lookupViaCity(city, fuzzy = false) {
+  const cities = cityMapping.filter((item) => {
+    if (!fuzzy) return item.city.toLowerCase() === city.toLowerCase()
+    return item.city.toLowerCase().includes(city.toLowerCase())
+  })
+
+  return cities
+}
+
+function lookupViaState(state, fuzzy = false) {
+  const states = cityMapping.filter((item) => {
+    if (!fuzzy) return item.province.toLowerCase() === state.toLowerCase()
+    return item.province.toLowerCase().includes(state.toLowerCase())
+  })
+
+  return states
+}
+
+function lookupViaCityState(city, state, fuzzy = false) {
+  const cities = cityMapping.filter((item) => {
+    let cityMatched = false
+    let stateMatched = false
+    if (!fuzzy) {
+      cityMatched = item.city.toLowerCase() === city.toLowerCase()
+      stateMatched = item.province.toLowerCase() === state.toLowerCase()
+
+      return cityMatched && stateMatched
+    }
+
+    cityMatched = item.city.toLowerCase().includes(city.toLowerCase())
+    stateMatched = item.province.toLowerCase().includes(state.toLowerCase())
+
+    return cityMatched && stateMatched
+  })
+
+  return cities
 }
 
 module.exports = {
   lookupViaCity
-};
+, lookupViaCityState
+, lookupViaState
+}
--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,65 @@
-const assert = require('assert');
-const cityTimezones = require('../index.js');
+'use strict'
+const assert = require('assert')
+const cityTimezones = require('../index.js')
 
 describe('City lookup tests', function() {
     it('finding Chicago', function() {
       let city = cityTimezones.lookupViaCity('Chicago')
-      console.log('found Chicago by name: ', city)
-      assert.equal(41.82999066, city[0].lat);
-    });
+      assert.equal(41.82999066, city[0].lat)
+    })
     it('return empty for non matching city', function() {
       let city = cityTimezones.lookupViaCity('Foobar')
-      assert.deepEqual([], city);
+      assert.deepEqual([], city)
     })
     it('return multiple matches', function() {
       let city = cityTimezones.lookupViaCity('Springfield')
-      console.log(city.length + ' matches found for Springfield')
-      assert(city.length > 1, city);
-    });
-});
+      assert(city.length > 1, city)
+    })
+    it('return multiple Jackson when fuzzy true', function() {
+      let city = cityTimezones.lookupViaCity('Jacks', true)
+      assert(city.length === 4)
+    })
+})
+
+describe('Province/State lookup tests', function() {
+  it ('finding Hawaii cities', function() {
+    let city = cityTimezones.lookupViaState('Hawaii')
+    assert(city.length === 6)
+  })
+  it('return empty for non matching province/state', function() {
+    let city = cityTimezones.lookupViaState('FizzBuzz')
+    assert.deepEqual([], city)
+  })
+  it('return all Mississippi when fuzzy true', function() {
+    let city = cityTimezones.lookupViaState('Missis', true)
+    assert(city.length === 11)
+  })
+})
+
+describe('City/State lookup tests', function() {
+  it('finding Los Angeles, California', function() {
+    let city = cityTimezones.lookupViaCityState('Los Angeles', 'California')
+    assert.equal(33.98997825, city[0].lat)
+    assert.equal('Los Angeles', city[0].city)
+  })
+
+  it('return empty for non matching city', function() {
+    let city = cityTimezones.lookupViaCityState('Foobar', 'Virginia')
+    assert.deepEqual([], city)
+  })
+
+  it('return empty for non matching state', function() {
+    let city = cityTimezones.lookupViaCityState('New York', 'FizzBuzz')
+    assert.deepEqual([], city)
+  })
+
+  it('find New York when fuzzy true', function() {
+    let city = cityTimezones.lookupViaCityState('New Yo', 'New York', true)
+    assert(city.length === 1)
+  })
+
+  it('find Denver when fuzzy', function() {
+    let city = cityTimezones.lookupViaCityState('Denver', 'Col', true)
+    assert(city.length === 1)
+  })
+})
